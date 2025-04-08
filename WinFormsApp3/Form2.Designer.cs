da0916f061529b88c38b5c0a58163778e1dc48ed
BRUNO GUIMARÃES SOUZA <194822024@ETEC.BR>
2025-04-08 09:47:16 -0300

    adicionei os arquivos básicos

---
+++ WinFormsApp3/Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(116, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(509, 50);
+            label2.TabIndex = 1;
+            label2.Text = "nosso projeto consiste em uma gerenciador de tarefas prático \r\nque possui o melhor das funcionalidades que vc precisa.";
+            label2.Click += label2_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+    }
+}
